b1523ee7a4271464f45f889a85b4ca58d3c12bf8
FacundoTorres <torresfdev@gmail.com>
2022-11-11 16:27:17 -0300

    se terminaron validaciones,sweet alert y funciones

--- main.js
+++ main.js
@@ -50,47 +50,148 @@
 
 // const cartas = ["As-Diamantes","2-Diamantes","3-Diamantes","4-Diamantes","5-Diamantes","6-Diamantes","7-Diamantes","8-Diamantes","9-Diamantes","10-Diamantes","J-Diamantes","Q-Diamantes","K-Diamantes","As-Corazones","2-Corazones","3-Corazones","4-Corazones","5-Corazones","6-Corazones","7-Corazones","8-Corazones","9-Corazones","10-Corazones","J-Corazones","Q-Corazones","K-Corazones","As-Picas","2-Picas","3-Picas","4-Picas","5-Picas","6-Picas","7-Picas","8-Picas","9-Picas","10-Picas","J-Picas","Q-Picas","K-Picas","As-Trebol","2-Trebol","3-Trebol","4-Trebol","5-Trebol","6-Trebol","7-Trebol","8-Trebol","9-Trebol","10-Trebol","J-Trebol","Q-Trebol","K-Trebol"]
 // class Jugador{
-//     constructor(nombre,apellido,edad,email,genero,alias,fichas){
-//     this.nombre = nombre
-//     this.apellido = apellido
-//     this.edad = edad
-//     this.email = email
-//     this.genero = genero
-//     this.alias = alias
-//     this.fichas=fichas
-// }}
-// const jugador2 = new Jugador ( "pedro", "picapiedra",29, "pepe@gmail.com", "masculino", "pedrito", 1500)
-// console.log(jugador2)
+    //     constructor(nombre,apellido,edad,email,genero,alias,fichas){
+        //     this.nombre = nombre
+        //     this.apellido = apellido
+        //     this.edad = edad
+        //     this.email = email
+        //     this.genero = genero
+        //     this.alias = alias
+        //     this.fichas=fichas
+        // }}
+        // const jugador2 = new Jugador ( "pedro", "picapiedra",29, "pepe@gmail.com", "masculino", "pedrito", 1500)
+        // console.log(jugador2)
+        
+        
+        // class Mazo{
+            //     constructor(cartas){
+                //         this.cartas = cartas;
+                //     }
+                
+                //     barajarCartas(){
+                    //     cartas.sort(()=> Math.random()- 0.5)
+                    //         return cartas;
+                    //     }
 
-
-// class Croupier{
-//     constructor(mezclar){
-//         this.mezclar = mezclar;
-//     }
-    
-//     barajarCartas(){
-//     cartas.sort(()=> Math.random()- 0.5)
-//         return cartas;
-//     }
-// }
-
-
-// const Croupier1 = new Croupier(cartas)
-// console.log(Croupier1.barajarCartas())
-
+                    //     repartir(cartas) {
+                        //     for (let i = 0; i <= 4; i++) {
+                            //     let seleccionarCarta = Math.floor(Math.random() * cartas.length);
+                            //     console.log("el croupier a sacado las siguienttes cartas : " + cartas[seleccionarCarta]);
+                            //     }
+                            //     }
+                            // }
+                            
+                            
+                            // const Mazo1 = new Mazo(cartas)
+                            // console.log(Mazo1.barajarCartas())
+                            
 document.getElementById('form').addEventListener('submit', (e) => {
     e.preventDefault();
-
-
-    let nombre = document.getElementById('nombre').value;
-    let apellido = document.getElementById('apellido').value;
-    let edad = document.getElementById('edad').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('passsword');
-
-    let expRegNombre = /^\s+$/;
-
-    if(nombre == null || nombre.lenght == 0 || expRegNombre.test(nombre)){
-        alert('nombre invalido')
+ 
+ 
+const nombre = document.getElementById('nombre').value.toLowerCase();
+const apellido = document.getElementById('apellido').value.toLowerCase();
+const edad = document.getElementById('edad').value;
+const email = document.getElementById('email').value.toLowerCase();
+const password = document.getElementById('password');
+ 
+const expReg = /^\s+$/;
+const expRegEmail =  !(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(email));
+ 
+    if(nombre == "" || nombre.lenght == 0 || nombre.lenght < 6 || expReg.test(nombre)){
+        Swal.fire({
+            position: 'center',
+            icon: 'warning',
+            title: 'El campo nombre no es valido',
+            showConfirmButton: false,
+            timer: 1000
+          })
+        document.getElementById('nombre')
+        return false;
     }
-})
+ 
+    else if(apellido == "" || apellido.lenght == 0 || expReg.test(apellido)){
+        Swal.fire({
+            position: 'center',
+            icon: 'warning',
+            title: 'El campo apellido no es valido',
+            showConfirmButton: false,
+            timer: 1000
+          })
+        document.getElementById('apellido')
+        return false;
+    }
+    else if (edad == "" || edad== 0 || isNaN(edad) ){
+        Swal.fire({
+            position: 'center',
+            icon: 'warning',
+            title: 'El campo edad no es valido.',
+            showConfirmButton: false,
+            timer: 1000
+          })
+        document.getElementById('edad')
+        return false;
+    }
+    else if(expRegEmail){
+        Swal.fire({
+            position: 'center',
+            icon: 'warning',
+            title: 'El campo email no es valido',
+            showConfirmButton: false,
+            timer: 1000
+          })
+        document.getElementById('email')
+        return false;
+    }
+    else{
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Se ha registrado con EXITO',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        document.getElementById('nombre').value="";
+        document.getElementById('apellido').value="";
+        document.getElementById('edad').value="";
+        document.getElementById('email').value="";
+        document.getElementById('password').value="";
+    }
+    })
+
+
+const simbolos = ["Corazones", "Trebol", "Picas", "Diamantes"]
+const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+const combinations = []
+
+class Mazo {
+    constructor(cartas = nuevoMazo()){
+    this.cartas=cartas}
+}
+
+class Carta{
+    contructor(simbolo,valor){
+        this.simbolo = simbolo
+        this.valor = valor
+    }
+}
+
+function nuevoMazo(){
+    simbolos.flatMap(p =>
+        valores.map(e=>combinations.push({combination: p + e})))
+}
+
+function mezclar(){
+    combinations.sort(()=> Math.random()- 0.5)
+    return combinations
+}
+
+
+
+const mazo = new Mazo()
+
+console.log (mezclar())
+
+
+
+
